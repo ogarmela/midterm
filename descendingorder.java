4e63633b7a957c546935289375e8d22b196dbacf
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-05-25 19:16:48 -0700

    Add files via upload

---
+++ descendingorder.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class descendingorder {
+	/ TODO: Write a void method selectionSortDescendTrace() that takes
+	// an integer array and the number of elements in the array as arguments,
+	// and sorts the array into descending order.
+	public static void selectionSortDescendTrace(int [] numbers, int numElements) {
+	  
+	}
+
+
+	public static void main(String[] args) {
+	Scanner scnr = new Scanner(System.in);
+	  
+	int input, i = 0;
+	int numElements = 0;
+	int [] numbers = new int[10];
+
+	// TODO: Read in a list of up to 10 positive integers; stop when
+	// -1 is read. Then call selectionSortDescendTrace() method.
+
+	}
+
+}
